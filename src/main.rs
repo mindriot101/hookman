c83a0f1f995161a00b8916874274f5b263033d32
Simon Walker <s.r.walker101@googlemail.com>
2021-07-19 08:29:30 +0100

    Adds initial command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+enum Commands {
+    /// Installs git hooks
+    Install {
+        /// Path of the configuration file.
+        #[structopt(short, long, default_value = ".hookman.toml")]
+        config: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Commands::from_args();
+    dbg!(opts);
 }
